90a96d46de61a40068a1ceed18e02cd20f7fd12b
Navyagayathri22 <2200031386cseh@gmail.com>
2025-06-22 16:39:19 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
-
+// Main.java
 public class Main {
     public static void main(String[] args) {
-        double pv = 1000.0; 
-        double rate = 0.05; 
-        int years = 5;       
+        Product[] products = {
+                new Product(105, "Laptop", "Electronics"),
+                new Product(101, "Shoes", "Fashion"),
+                new Product(110, "Watch", "Accessories"),
+                new Product(102, "Phone", "Electronics")
+        };
 
-        double result = Forecast.futureValue(pv, rate, years);
-        System.out.printf("Future Value after %d years: %.2f\n", years, result);
+        System.out.println("Linear Search");
+        Product found1 = Search.linearSearch(products, 110);
+        System.out.println(found1 != null ? found1 : "Product not found");
+
+        System.out.println("\nBinary Search");
+        Search.sortById(products);
+        Product found2 = Search.binarySearch(products, 110);
+        System.out.println(found2 != null ? found2 : "Product not found");
     }
 }
