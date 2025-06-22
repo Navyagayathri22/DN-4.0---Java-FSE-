059b7784d2c036ff1c2f5d90a7a9cc3a674d58cf
Navyagayathri22 <2200031386cseh@gmail.com>
2025-06-22 16:27:29 +0530

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,38 @@
+// SearchUtil.java
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Search{
+
+    // Linear Search by productId
+    public static Product linearSearch(Product[] products, int targetId) {
+        for (Product p : products) {
+            if (p.productId == targetId) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    // Binary Search (array must be sorted by productId)
+    public static Product binarySearch(Product[] products, int targetId) {
+        int low = 0, high = products.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (products[mid].productId == targetId) {
+                return products[mid];
+            } else if (products[mid].productId < targetId) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return null;
+    }
+
+    // Sort array by productId
+    public static void sortById(Product[] products) {
+        Arrays.sort(products, Comparator.comparingInt(p -> p.productId));
+    }
+}
