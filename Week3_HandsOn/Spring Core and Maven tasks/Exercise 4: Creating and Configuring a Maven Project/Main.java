f965f50cf86a6c257af11b8707e86a4dc6d75503
Navyagayathri22 <2200031386cseh@gmail.com>
2025-07-06 17:50:25 +0530

    commit

---
+++ Week3_HandsOn/Spring Core and Maven tasks/Exercise 4: Creating and Configuring a Maven Project/Main.java
@@ -0,0 +1,21 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main
+
+
+{
+    public static void main(String[] args) {
+        // Load Spring context
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get bookService bean from context
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Test method call
+        bookService.addBook("Effective Java");
+    }
+}
