41e5cadd1b8309ae713f916bda0313bf4f82c90f
Navyagayathri22 <2200031386cseh@gmail.com>
2025-07-06 23:05:21 +0530

    commit

---
+++ Week3_HandsOn/Spring Data With Hibernate/Spring Data JPA- Quick Example/CountryServiceImpl.java
@@ -0,0 +1,21 @@
+package com.cognizant.ormlearn.service;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CountryServiceImpl implements CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Override
+    public Country findCountryByCode(String code) {
+        Optional<Country> country = countryRepository.findById(code);
+        return country.orElse(null);
+    }
+}
