269f0f23b77762c6ea634e5ec3ce9ea5a85235b8
Navyagayathri22 <2200031386cseh@gmail.com>
2025-07-06 22:54:40 +0530

    commit

---
+++ Week3_HandsOn/Spring Data With Hibernate/Spring Data JPA- Quick Example/OrmLearnApplication.java
@@ -0,0 +1,26 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @PostConstruct
+    public void testCountry() {
+        Country country = countryService.findCountryByCode("IN");
+        System.out.println("Country: " + country);
+    }
+}
